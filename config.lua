2595790d3125971378b0fa1712107f7a2b8091f7
Jason <Jasondeklein1603@gmail.com>
2022-06-28 12:06:33 +0200

    2 naar 1

--- config.lua
+++ config.lua
@@ -9,5 +9,5 @@ Config.EnableDeleteButton = true -- Define if the player can delete the characte
 
 Config.DefaultNumberOfCharacters = 5 -- Define maximum amount of default characters (maximum 5 characters defined by default)
 Config.PlayersNumberOfCharacters = { -- Define maximum amount of player characters by rockstar license (you can find this license in your server's database in the player table)
-    { license = "license:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", numberOfChars = 2 },
+    { license = "license:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", numberOfChars = 1 },
 }
